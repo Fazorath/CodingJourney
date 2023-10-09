9b9c678fe927bc40fb9ba41af50b37e929f9075a
Yoenis Hernandez <40134268+Fazorath@users.noreply.github.com>
2023-10-09 13:57:52 -0400

    1cypher problems

--- Python/Project4/cypher.py
+++ Python/Project4/cypher.py
@@ -1,5 +1,16 @@
-def make_shirt(size,text):
-    print(f"The first Shirt is going to be: {size}")
-    print(f"The text on the shirt is going to be: {text}")
+## Yoenis Hernandez
+## COP 2002
+## October 4, 2023
+## Ceasar Cipher
+## A program that replaces the indexes of the text you provide with another index from another list.
 
-make_shirt('large','i hate you')
+
+# 2 lists and checking to see if they have the same lenght
+Alphabet1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+Alphabet2 = ["B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",  "t", "u", "v", "w", "x", "y", "z", "a"]
+print(Alphabet1)
+print(Alphabet2)
+
+shift the values of the string of the user input
+# ShiftInput = input(int("What is your Shift Amount: "))
+
