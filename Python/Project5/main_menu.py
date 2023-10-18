64f00a6d0458acadf46284f711961fb01e1d92de
Your Name <hernandezyoenis@gmail.com>
2023-10-17 22:42:58 -0400

    getinput() function finished starting on others. Python

---
+++ Python/Project5/main_menu.py
@@ -0,0 +1,23 @@
+## Arrays
+PortNums = [20,21,22,23,25,53,67,68,80,110,137,139,143,161,162,389,443,445,3389]
+
+## Main Menu FUnction
+def getinput():
+ choice = 0
+ while choice == 0:
+  print("1. Given the Port Number, Identify the Protocol")
+  print("2. Given the Port Protocol, Identify the Port Number")
+  print("3. Exit\n")
+  
+  choice = int(input("Choice: "))
+  
+  if choice == 1:
+    print("\nIdentify Port Number") 
+  elif choice == 2:
+    print("\nIdentify Port Protocol")
+  elif choice == 3:
+    print("\nHopefully this program helped in studying !!")
+  else:
+    choice = 0
+    print("\nInvalid Choice Choose 1 - 2 - 3 please :) ")
+getinput()
