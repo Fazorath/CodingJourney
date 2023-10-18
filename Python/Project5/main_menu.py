5f8aa3a2e49e1864e148d0e53940bc8bd2e37ffb
Your Name <hernandezyoenis@gmail.com>
2023-10-17 22:47:46 -0400

    Both Arrays are done

--- Python/Project5/main_menu.py
+++ Python/Project5/main_menu.py
@@ -1,7 +1,10 @@
 ## Arrays
 PortNums = [20,21,22,23,25,53,67,68,80,110,137,139,143,161,162,389,443,445,3389]
+PortProto = ['FTP','FTP','SSH','Telnet','SMTP','DNS','DHCP','DHCP','HTTP','POP3','netBIOS','netBIOS','IMAP','SNMP','SNMP','LDAP','HTTPS','SMB','RDP']
+print(PortProto[2])
 
-## Main Menu FUnction
+
+## Main Menu Function
 def getinput():
  choice = 0
  while choice == 0:
@@ -20,4 +23,4 @@ def getinput():
   else:
     choice = 0
     print("\nInvalid Choice Choose 1 - 2 - 3 please :) ")
-getinput()
+# getinput()
