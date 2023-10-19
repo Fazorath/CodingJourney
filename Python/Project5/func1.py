9305bfbf98fcd0c119ab6df2b84d2edc0ad9e9c4
Your Name <hernandezyoenis@gmail.com>
2023-10-18 21:45:26 -0400

    random and answer linked. if else finished.

--- Python/Project5/func1.py
+++ Python/Project5/func1.py
@@ -6,17 +6,22 @@ portNumArray = [20,21,22,23,25,53,67,68,80,110,137,139,143,161,162,389,443,445,3
 portNameArray = ['FTP','FTP','SSH','Telnet','SMTP','DNS','DHCP','DHCP','HTTP','POP3','netBIOS','netBIOS','IMAP','SNMP','SNMP','LDAP','HTTPS','SMB','RDP']
 
 
-randPort = random.choice(portNumArray)
-print(randPort) ## Can we use choice or only randrange
+
 
 def numToName(portNumber,portNumArray=[20,21,22,23,25,53,67,68,80,110,137,139,143,161,162,389,443,445,3389],portNameArray=['FTP','FTP','SSH','Telnet','SMTP','DNS','DHCP','DHCP','HTTP','POP3','netBIOS','netBIOS','IMAP','SNMP','SNMP','LDAP','HTTPS','SMB','RDP']
-,): # Array of Num and Array of Name Player input
-  ## Choose a random port Number
+,): # Array of Num and Array of Name Player input portnumber solved externally
   ## display message with the random port number
-  playeranswer = input(f"What is the Protocol name for this Port Number {portNumber}: ")
-  ## ask user for their choice
-  ## Check if their choice was right
-  ## if right 
+  correctAns = portNameArray[portNumber] # Which index in the array the correct answer is
+  randomport = portNumArray[portNumber] # Random number is index of num array with the number generated
+  
+  # User Choice
+  playeranswer = input(f"What is the Protocol name for this Port Number {randomport}: ")
+  if playeranswer == correctAns:
+    print("Congrats youre a genius")
+  else:
+    print("try Again")
+  print(correctAns)
+
   ## else
 
-numToName(randPort)
+
--- Python/Project5/main_menu.py
+++ Python/Project5/main_menu.py
@@ -1,39 +1,45 @@
-from random import randrange
 import random
 from func1 import numToName
 
 # Main Menu Function
 
 
+
 def main():
-    def getinput():
-        choice = 0
-        while choice == 0:
-            print("1. Given the Port Number, Identify the Protocol")
-            print("2. Given the Port Protocol, Identify the Port Number")
-            print("3. Exit\n")
-            portNumArray = [20, 21, 22, 23, 25, 53, 67, 68, 80,
-                             110, 137, 139, 143, 161, 162, 389, 443, 445, 3389]
-            portNameArray = ['FTP', 'FTP', 'SSH', 'Telnet', 'SMTP', 'DNS', 'DHCP', 'DHCP', 'HTTP',
-                              'POP3', 'netBIOS', 'netBIOS', 'IMAP', 'SNMP', 'SNMP', 'LDAP', 'HTTPS', 'SMB', 'RDP']
+    choice = 0
+    while choice == 0:
+        print("Main Menu")
+        print("1. Given the Port Number, Identify the Protocol")
+        print("2. Given the Port Protocol, Identify the Port Number")
+        print("3. Exit\n")
+        portNumArray = [20, 21, 22, 23, 25, 53, 67, 68, 80,
+                         110, 137, 139, 143, 161, 162, 389, 443, 445, 3389]
+        portNameArray = ['FTP', 'FTP', 'SSH', 'Telnet', 'SMTP', 'DNS', 'DHCP', 'DHCP', 'HTTP',
+                          'POP3', 'netBIOS', 'netBIOS', 'IMAP', 'SNMP', 'SNMP', 'LDAP', 'HTTPS', 'SMB', 'RDP']
+        # Calling get input Function
+        getinput()
+        
 
-            choice = (input("Choice: "))
 
-            if choice == '1':
-                print("Identify Port Number:\n")
-                randport = (random.randrange(1,19))
-                numToName(randport)
-                ## Call num to name function 
-            elif choice == '2':
-                print("Identify Port Protocol:\n")
-            elif choice == '3':
-                print("Hopefully this program helped in studying !!")
-            elif choice == 'm':
-                getinput() ## Loops if user presses M as their Choice
-            else:
-                print("Try one more time with letter bucko \n")
-                getinput()
-            return choice
-    getinput()
+def getinput():
+    
+    choice = (input("Choice: "))
+    if choice == '1':
+        print("Identify Port Number:\n")
+        randport = random.randrange(1,19)
+        numToName(randport) ## Calling numtoName
+    elif choice == '2':
+        print("Identify Port Protocol:\n")
+        randName = random.randrange(1,19)
+    elif choice == '3':
+        print("Hopefully this program helped in studying !!")
+    elif choice == 'm':
+        print("Main menu:")
+        getinput() ## Loops if user presses M as their Choice
+    else:
+        print("Try one more time with one of the numbers mane  \n")
+        getinput()
+    return choice
+    
 
 main()
