90ea6bb608ba7a3b84f9112a1745e8f3213f3777
Your Name <hernandezyoenis@gmail.com>
2023-10-18 20:15:33 -0400

    struggling still with trying to scramble

---
+++ Python/Project5/func1.py
@@ -0,0 +1,22 @@
+## Function one is 
+## def numToName(portNumArray,portNameArray,portNumber):
+
+## Arrays
+portNumArray = [20,21,22,23,25,53,67,68,80,110,137,139,143,161,162,389,443,445,3389]
+portNameArray = ['FTP','FTP','SSH','Telnet','SMTP','DNS','DHCP','DHCP','HTTP','POP3','netBIOS','netBIOS','IMAP','SNMP','SNMP','LDAP','HTTPS','SMB','RDP']
+
+
+
+
+def numToName(portNumber,portNumArray=[20,21,22,23,25,53,67,68,80,110,137,139,143,161,162,389,443,445,3389],portNameArray=['FTP','FTP','SSH','Telnet','SMTP','DNS','DHCP','DHCP','HTTP','POP3','netBIOS','netBIOS','IMAP','SNMP','SNMP','LDAP','HTTPS','SMB','RDP']
+,): # Array of Num and Array of Name Player input
+  ## Choose a random port Number
+  
+  ## display message with the random port number
+  playeranswer = input(f"What is the Protocol name for this Port Number {portNumber}: ")
+  ## ask user for their choice
+  ## Check if their choice was right
+  ## if right 
+  ## else
+
+numToName(33)
--- Python/Project5/main_menu.py
+++ Python/Project5/main_menu.py
@@ -1,38 +1,33 @@
-import random
+from func1 import numToName
 
-## Arrays
-portNumArray = [20,21,22,23,25,53,67,68,80,110,137,139,143,161,162,389,443,445,3389]
-portNameArray = ['FTP','FTP','SSH','Telnet','SMTP','DNS','DHCP','DHCP','HTTP','POP3','netBIOS','netBIOS','IMAP','SNMP','SNMP','LDAP','HTTPS','SMB','RDP']
+# Main Menu Function
 
 
-## numToName(portNumArray, portNameArray, portNumber) 
-## - which uses the portNumber to find it in the portNumArray  and find the corresponding port name in the portNameArray.
-def numToName(portNumArray,portNameArray,portNumber):
-  ans = ''
-  indexnum = random.randrange(0,20)
-  correct = portNameArray[indexnum]
-  print(f"What is the protocol for Port Num {indexnum}? ")
-  print(correct)
+def main():
+    def getinput():
+        choice = 0
+        while choice == 0:
+            print("1. Given the Port Number, Identify the Protocol")
+            print("2. Given the Port Protocol, Identify the Port Number")
+            print("3. Exit\n")
+            portNumArray = [20, 21, 22, 23, 25, 53, 67, 68, 80,
+                            110, 137, 139, 143, 161, 162, 389, 443, 445, 3389]
+            portNameArray = ['FTP', 'FTP', 'SSH', 'Telnet', 'SMTP', 'DNS', 'DHCP', 'DHCP', 'HTTP',
+                             'POP3', 'netBIOS', 'netBIOS', 'IMAP', 'SNMP', 'SNMP', 'LDAP', 'HTTPS', 'SMB', 'RDP']
 
-numToName(portNumArray,portNameArray,22)
+            choice = int(input("Choice: "))
 
-## Main Menu Function
-def getinput():
- choice = 0
- while choice == 0:
-  print("1. Given the Port Number, Identify the Protocol")
-  print("2. Given the Port Protocol, Identify the Port Number")
-  print("3. Exit\n")
-  
-  choice = int(input("Choice: "))
-  
-  if choice == 1:
-    print("\nIdentify Port Number") 
-  elif choice == 2:
-    print("\nIdentify Port Protocol")
-  elif choice == 3:
-    print("\nHopefully this program helped in studying !!")
-  else:
-    choice = 0
-    print("\nInvalid Choice Choose 1 - 2 - 3 please :) ")
-# getinput()
+            if choice == 1:
+                print("\nIdentify Port Number:\n")
+                numToName(portNumArray, portNameArray, portNumber)
+            elif choice == 2:
+                print("\nIdentify Port Protocol:\n")
+            elif choice == 3:
+                print("\nHopefully this program helped in studying !!")
+            else:
+                choice = 0
+                print("\nInvalid Choice Choose 1 - 2 - 3 please :) ")
+    getinput()
+
+
+main()
