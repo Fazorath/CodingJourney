288f5dcca68019fc7531c73f7e4dad86ed4a3039
Your Name <hernandezyoenis@gmail.com>
2023-10-20 16:42:26 -0400

    some progress strugling with asking q again

--- Python/Studying/Lectures/10-17-23/generallayaout.py
+++ Python/Studying/Lectures/10-17-23/generallayaout.py
@@ -10,24 +10,26 @@ protocol = ['FTP', 'FTP', 'SSH', 'Telnet', 'SMTP', 'DNS', 'DHCP', 'DHCP', 'HTTP'
 def numToName(portNumber,portNumArray=[20,21,22,23,25,53,67,68,80,110,137,139,143,161,162,389,443,445,3389], portNameArray=['FTP', 'FTP', 'SSH', 'Telnet', 'SMTP', 'DNS', 'DHCP', 'DHCP', 'HTTP',
             'POP3', 'netBIOS', 'netBIOS', 'IMAP', 'SNMP', 'SNMP', 'LDAP', 'HTTPS',
             'SMB', 'RDP']): 
-  randomnumber = portNumArray[portNumber]
-  correct = portNameArray[portNumber]
   iscorrect = True
-  question = input(f"What is the Port Protocol to this Port Number( m to menu ) {randomnumber}: ")
+  while iscorrect == False:
+      randnumber()
+      question
   while iscorrect == True:
-   if (question==correct):
-       print("Correct!!\nHere is another one:\n")
-       randnumber()
-       numToName(randnumber())
-   elif(question==''):
-       nameToNum(randnumber())
-   elif( question == 'm'):
+    randomnumber = portNumArray[portNumber]
+    correct = portNameArray[portNumber]
+    question = input(f"What is the Port Protocol to this Port Number( m to menu ) {randomnumber}: ")
+    while(question==correct):
+        print("Correct")
+        break
+    if(question==''):
+       question
+       continue
+    elif( question == 'm'):
        print(f"Choice: menu")
        break
-   elif (question != correct):
-       print("Sorry, Not Quite Right !\nTry Again:\n")
-       randnumber()
-       numToName(randnumber())
+    elif (question != correct):
+       print(f"Sorry, Not Quite Right: {correct} !\nTry Again:\n")
+       iscorrect == False
  # which uses the portNumber to find it in the portNumArray  and find the corresponding port name in the portNameArray.
 
 
@@ -37,20 +39,20 @@ def nameToNum(portNumber,portNumArray=[20,21,22,23,25,53,67,68,80,110,137,139,14
             'SMB', 'RDP']):
   randomname = portNameArray[portNumber]
   correctnum = portNumArray[portNumber]
+  print(correctnum)
   iscorrect = True
   question = int(input(f"What is the Correct Port Number for this Protocol - {randomname}: "))
   
-  while(iscorrect==True):
+  while(question):
       if(question==correctnum):
           print("Correct!!\nHere is another one:\n")
           randnumber()
-          nameToNum(randnumber())
       elif(question==''):
-          nameToNum(randnumber())
+          question
       elif(question=="m"):
           print(f"Choice: Menu") 
           break
-      elif(question!=correctnum):
+      else:
           print("\nSorry, Not Quite Right !\nTry Again:\n")
           randnumber()
           nameToNum(randnumber())
@@ -80,8 +82,8 @@ def ifusr(userchoice):
    ## USER INPUT
    if userchoice == '1':
       print("\nChoice 1: Identifiy Port Protocol\n")
-      randnumber()
-      numToName(randnumber())
+      randomPort = randrange(1,19)
+      numToName(randomPort)
    elif userchoice == '2':
       print("\nChoice 2: Identifiy Port Name\n")
       randnumber()
