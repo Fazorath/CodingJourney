afeb5f4e095343d2d5ad647ffeea61dad9bd051b
Your Name <hernandezyoenis@gmail.com>
2023-10-22 19:17:05 -0400

    different ways of opening file

--- Python/Studying/Lectures/10-17-23/read/read.py
+++ Python/Studying/Lectures/10-17-23/read/read.py
@@ -1,9 +1,14 @@
+#  reading inside a file
 file=''
 with open("/Users/exempt/Repo Codespace/CodingJourney/Python/Studying/Lectures/10-17-23/read/list.txt") as read_file:
-  file = read_file.read()
-print(file)
+  file = read_file.readlines()
+  file2 = read_file.read()
+for item in file:
+  print(item)
 
-      
+for item in file2:
+  print(item)
+
 
 
 
