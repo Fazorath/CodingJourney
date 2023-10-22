986f79c8092dd3b36021957b974696b5863359c6
Your Name <hernandezyoenis@gmail.com>
2023-10-22 18:57:25 -0400

    read files

---
+++ Python/Studying/Lectures/10-17-23/read/read.py
@@ -0,0 +1,15 @@
+file=''
+with open("/Users/exempt/Repo Codespace/CodingJourney/Python/Studying/Lectures/10-17-23/read/list.txt") as read_file:
+  file = read_file.read()
+print(file)
+
+      
+
+
+
+
+
+
+
+
+
