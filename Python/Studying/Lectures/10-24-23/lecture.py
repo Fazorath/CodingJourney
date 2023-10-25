02a9a6794849c7976d3c3d3f6288b0a283279231
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2023-10-24 19:18:31 -0700

    Try Except Else Example

--- Python/Studying/Lectures/10-24-23/lecture.py
+++ Python/Studying/Lectures/10-24-23/lecture.py
@@ -1,2 +1,16 @@
 ## Lecture 10/24/23
-## Fiscussing the past Project but i aced that shit so ??
+## discussing the past Project but i aced that shit so ??
+
+## Exception Handling
+path = "CodingJournPython\\Studying\\Lectures\\10-24-23\\example.txt"
+try: # code that you expect to fail or have some error
+    with open(path) as f:
+        content = f.readlines()
+except: # What to do when the code fails
+    print(f"The file you are looking for: {path}\nDoes Not Exist!")
+else: # If the code Succeeds to the following
+    index = 0
+    for line in content:
+        line = line.rstrip()
+        print(f"{index}: {line}")
+        index = index + 1
