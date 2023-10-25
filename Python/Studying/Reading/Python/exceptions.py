4dc0a239cbc4910577e820d199eb7eed0f7d1ffe
Your Name <hernandezyoenis@gmail.com>
2023-10-24 23:01:56 -0400

    Zero division in a while loop

--- Python/Studying/Reading/Python/exceptions.py
+++ Python/Studying/Reading/Python/exceptions.py
@@ -1,1 +1,27 @@
 ## Handling Exceptions
+## Handled using Try and Except blocks
+
+## ZeroDivisionError
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+
+print("Give me 2 numbers, and ill divide them")
+print("Enter 'q' to quit")
+
+
+# Incorporating ZeroDivisionError into a while loop
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("\nSecond number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number)/ int(second_number)
+    except ZeroDivisionError:
+        print("You cannot divide by 0")
+    else:
+        print(answer)
