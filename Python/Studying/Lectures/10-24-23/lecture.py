5d88510fd000e3de91452bfcaba2e6b36f9b7927
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2023-10-24 19:22:33 -0700

    File not Found Zero Division

--- Python/Studying/Lectures/10-24-23/lecture.py
+++ Python/Studying/Lectures/10-24-23/lecture.py
@@ -3,12 +3,14 @@
 
 ## Exception Handling
 path = "CodingJournPython\\Studying\\Lectures\\10-24-23\\example.txt"
-# print(9/0)
 try: # code that you expect to fail or have some error
+    print(9/0)
     with open(path) as f:
         content = f.readlines()
-except: # What to do when the code fails
+except FileNotFoundError: # What to do when the code fails
     print(f"The file you are looking for:\n\n{path}\n\nDoes Not Exist!")
+except ZeroDivisionError:
+    print(f"\n\nYou divided by zero stupid")
 else: # If the code Succeeds to the following
     index = 0
     for line in content:
