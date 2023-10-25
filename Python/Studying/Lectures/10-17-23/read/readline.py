e5f04c6b1313006ceeb03555d31d2a35fc634b54
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2023-10-24 19:09:28 -0700

    readline and using .rstrip

--- Python/Studying/Lectures/10-17-23/read/readline.py
+++ Python/Studying/Lectures/10-17-23/read/readline.py
@@ -2,4 +2,9 @@ path = 'CodingJourney\\Python\\Studying\\Lectures\\10-17-23\\read\\list.txt'
 
 with open(path) as f: #With Open
     content = f.readlines()
-    print(content)
+    print(f"{content}\n\n\n")
+    print("Using the for line loop and .rstrip()\nWill make the dictionary into line by line str")
+    for line in content:
+        line = line.rstrip()
+        print(line)
+    print(type(line))
