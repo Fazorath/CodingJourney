187031ec4518814eaf722eeb471dd6ca762fa3f4
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2023-10-24 19:01:39 -0700

    Lecture Help. File path. double slashes

--- Python/Studying/Lectures/10-17-23/read/read.py
+++ Python/Studying/Lectures/10-17-23/read/read.py
@@ -1,7 +1,7 @@
 #  reading inside a file
 file=''
-with open("/Users/exempt/Repo Codespace/CodingJourney/Python/Studying/Lectures/10-17-23/read/list.txt",'a') as read_file:
-  read_file.write('new line that is appened to the file\n')
+with open("CodingJourney\\Python\\Studying\\Lectures\\10-17-23\\read\\list.txt",'a') as read_file:
+  read_file.write('I have to Double Slash the entire path for it to work lol.\n')
 print(file)
 
 
---
+++ Python/Studying/Lectures/10-24-23/lecture.py
@@ -0,0 +1,2 @@
+## Lecture 10/24/23
+## Fiscussing the past Project but i aced that shit so ??
