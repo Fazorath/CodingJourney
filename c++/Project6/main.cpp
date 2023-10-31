64164c0466d943c1e7ad7dd465967ed9c88f4ff2
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2023-10-31 15:48:23 -0400

    FINAL TOUCHES I THINK

--- c++/Project6/main.cpp
+++ c++/Project6/main.cpp
@@ -11,57 +11,66 @@
 // Had alot of fun enjoying using functions and even went ahead and looked at how to summon them from other files meaning i can do this with only
 // my main function and just #include <filename> makes it look really nice.
 
-
 #include <iostream>
 using namespace std;
 
-
-int menu (){
+// Menu function to call options
+int menu()
+{
     int answer;
-    while (answer != 4 && answer != 3 && answer != 2 && answer != 1)
-        {
+
+    while (answer != 4 && answer != 3 && answer != 2 && answer != 1) // while loop to validate the input
+    {
         cout << "Geometry Calculator" << endl;
         cout << "1. Calculate the area of a Circle" << endl;
         cout << "2. Calculate the area of a Rectangle" << endl;
         cout << "3. Calculate the area of a triangle" << endl;
         cout << "4. Quit" << endl;
-        cout << endl << "Enter your choice (1-4): ";
+        cout << endl
+             << "Enter your choice (1-4): ";
         cin >> answer;
-        }
-        return answer; /* code */
+    }
+
+    return answer; /* code */
 }
 
-float circlearea(){
+// Function to figure out circle area
+float circlearea()
+{
     float radius;
-    cout << endl << "Enter the radius of the circle: ";
+    cout << endl
+         << "Enter the radius of the circle: ";
     cin >> radius;
-    while (radius < 0)
+    while (radius < 0) // Checking for negative numbers
     {
         cout << "Invalid input" << endl;
-        cout << endl << "Enter the radius of the circle: ";
-        cin >> radius; // Ask for radius again
+        cout << endl
+             << "Enter the radius of the circle: ";
+        cin >> radius;
     }
-    float area = 3.14159 * radius * radius; // No need to declare area again
-    cout <<endl << "The area of the circle is " << area << endl;
+    float area = 3.14159 * radius * radius; // The math
+    cout << endl
+         << "The area of the circle is " << area << endl;
     return area;
 }
 
-// Starting the function for the Area of a Rectangle 
-float rectanglearea(){
+// Function to figure out rectangle area
+float rectanglearea()
+{
     float length;
     float height;
-    cout << endl <<"Enter the length of the rectangle: ";
+    cout << endl
+         << "Enter the length of the rectangle: ";
     cin >> length;
-
-   
-    while (length < 0 )
+    while (length < 0) // Checking for Negative Numbers
     {
         cout << "invalid length. Input again: ";
         cin >> length;
-    }  
-    cout << endl <<"Enter the Height of the rectangle: ";
+    }
+    cout << endl
+         << "Enter the Height of the rectangle: ";
     cin >> height;
-    while (height < 0 )
+    while (height < 0)
     {
         cout << "invalid Height. Input again: ";
         cin >> height;
@@ -69,23 +78,27 @@ float rectanglearea(){
     float area = length * height;
     cout << endl;
     cout << endl;
-    cout << "L:" << length << "  H:" << height << endl;
+    cout << "L:" << length << "  H:" << height << endl; // My little QOL change to see your final inputs
     cout << "The area of the rectangle is " << area << endl;
     return area;
 }
 
-float trianglearea(){ // New Triangle Function
+// Function to figure out triangle area
+float trianglearea()
+{
     float base;
     float height;
 
-    cout << endl << "Enter the base of the triangle: ";
+    cout << endl
+         << "Enter the base of the triangle: ";
     cin >> base;
-    while (base < 0 ) // While loop to validate the base
+    while (base < 0) // While loop to validate the base
     {
         cout << "Invalid base. Input again: ";
         cin >> base;
     }
-    cout << endl << "Enter the height of the triangle: ";
+    cout << endl
+         << "Enter the height of the triangle: ";
     cin >> height;
     while (height < 0) // While loop to validate the height
     {
@@ -96,34 +109,48 @@ float trianglearea(){ // New Triangle Function
     cout << endl;
     cout << endl;
     cout << "B:" << base << "  H:" << height << endl;
-    float area = (base * height) / 2;    
+    float area = (base * height) / 2;
     cout << "The area of the Triangle is: " << area << endl;
     return area;
 }
 
-void uservalidate(int answer){
-   if (answer == 1){
-       cout << endl << "You chose: Area of a Circle using Radius" << endl;
-       circlearea();
-   }
-   else if (answer == 2){
-        cout << endl << "You chose: Area of a Rectangle using Lenght and Width" << endl;
+// Function to validate the user input
+void uservalidate(int answer)
+{
+    if (answer == 1) // Validation for the user input
+    {
+        cout << endl
+             << "You chose: Area of a Circle using Radius" << endl; // Letting user know which they have chosen
+        circlearea();                                               // Call Approriate function
+    }
+    else if (answer == 2)
+    {
+        cout << endl
+             << "You chose: Area of a Rectangle using Lenght and Width" << endl;
         rectanglearea();
-   }
-   else if (answer == 3){
-       cout << endl << "You chose: Area of a Triangle using Base and Height" << endl;
-       trianglearea(); // calling function in the my user validation to start func
-   }
-   else if (answer == 4){
-       cout << endl << "You chose: 4" << endl;
-   }
-   else {
-       cout << endl << "Invalid input" << endl;
-   }
+    }
+    else if (answer == 3)
+    {
+        cout << endl
+             << "You chose: Area of a Triangle using Base and Height" << endl;
+        trianglearea();
+    }
+    else if (answer == 4)
+    {
+        cout << endl
+             << "You chose: 4" << endl;
+    }
+    else
+    {
+        cout << endl
+             << "Invalid input" << endl;
+    }
 }
 
-int main(){
-    int choice = menu();
-    uservalidate(choice);
+// Main function that intializes the program
+int main()
+{
+    int choice = menu();  // Call menu function and assign to int variable
+    uservalidate(choice); // Call uservalidate function and pass the int variable. does its thang
     return 0;
 }
