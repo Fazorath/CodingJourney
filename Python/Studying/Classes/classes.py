9150a457f439d790300eae00b5ed1dc959cee29f
Your Name <hernandezyoenis@gmail.com>
2023-11-06 19:01:18 -0500

    Added instances and methods to Dog class PLus 9-1 From the book

--- Python/Studying/Classes/classes.py
+++ Python/Studying/Classes/classes.py
@@ -13,3 +13,15 @@ class Dog:  # Define a class called dog / Capital represents a class
     def roll_over(self):
         '''Simulate rolling over in response to a command'''
         print(f"{self.name} rolled over!")
+
+Dog1 = Dog('Willie', 6) # Create an instance of a class 
+Dog2 = Dog('Lucy', 3) # Create an instance of a class
+
+
+print(f"My dog's name is {Dog1.name}.") # Accessing attributes
+print(f"My dog is {Dog1.age} years old.") # Accessing attributes
+Dog1.sit() # Calling methods    
+print("\n")
+print(f"My dog's name is {Dog2.name}.") # Accessing attributes
+print(f"My dog is {Dog2.age} years old.") # Accessing attributes
+Dog2.sit() # Calling methods
