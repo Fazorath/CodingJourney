683070970f4fbe4f50ce25c6773ec6fb40872d33
Your Name <hernandezyoenis@gmail.com>
2023-11-14 22:32:46 -0500

    Remove unnecessary VSCode configuration files and update binary files in c++ projects.

---
+++ c++/Project7/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+// Write a C++ program that demonstrates a class named Dice.
+// It will use the random number generator to simulate rolling a 6-sided Die 100 times, 
+// while counting how many times each number of the die comes up.
+//
+// Example output:
+// The number 1 was rolled 23 times
+// The number 2 was rolled 15 times
+
+
+
+int main() {
+    std::cout << "Hello World!\n";
+class Circle {
+    private:
+        double radius;
+    public:
+        void setRadius(double r);
+        double calcArea();
+};
+
+void Circle::setRadius(double r) {
+    radius = r;
+}
+
+double Circle::calcArea() {
+    return 3.14159 * pow(radius,2);
+}
+}
