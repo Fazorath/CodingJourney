be4151a8717f080f27c7a715121f34de9f3facde
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2023-11-15 14:43:03 -0500

    Got the c++ working but not random and rolling more than 100 times

--- c++/Learning Classes/main2.cpp
+++ c++/Learning Classes/main2.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib> 
 #include <iostream>
 using namespace std;
 
@@ -7,18 +8,25 @@ const int sides = 6;
 class Dice {
     private:
         int dice[sides];
-        int runs;
+        int numberofrolls = 100;
     public:
-        void roll();
-        
+        void roll()
+        {
+        for(int i = 0; i < numberofrolls; ++i){
+            int roll = rand() % sides;
+            dice[roll] += 1;
+            }
+        }
 
-        void display();
+        void display(){
+            for(int i = 0; i < sides; ++i){
+                cout << "The number " << i+1 << " was rolled " << dice[i] << " times" << endl;
+            }
+        }
 };
 
 int main(){
-    HelloWorld hw;
-    hw.sayHello();
-    hw.sayGoodbye();
-    hw.saySomething("Yannah Blaise");
-    return 0;
+  class Dice d1;
+  d1.roll();
+    d1.display();
 }
