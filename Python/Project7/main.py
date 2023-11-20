4225dc2aa13b4bfa94e9480d8075efb1bab3747f
Your Name <hernandezyoenis@gmail.com>
2023-11-20 13:31:01 -0500

    json test dump worked

--- Python/Project7/main.py
+++ Python/Project7/main.py
@@ -4,18 +4,27 @@
 ## for each row of data that is not the header row.  JavaScript is a scripting language used for web pages.  
 ## JSON is a common format used to transmit data for web apps from a web server to a client 
 ## (i.e. such as from sfcollege.edu's web server to your computer) so it can be displayed in a browser.
+import json
+
 def opencsv():
     filename=input("Enter the file Name: ")
     with open(filename, 'r') as file:
         for line in file:
             content = file.readlines()
-            print("\n")
-            print(content[0])
-            print(content[1])
-            print(content[2])
+            print(content)
+            print("\n\n")
+        return content
+
+def createJSON(content):
+    jsonname = input("Enter the file Name: ")
+    with open(jsonname, 'w') as file:
+        json.dump(content, file)
+
+
 
 def main():
-    opencsv()
+    content = opencsv()
+    createJSON(content)
 
 if __name__ == "__main__":
     main()
