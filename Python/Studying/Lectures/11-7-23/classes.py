40105dc79c167650f1aea334c0ce79e0abcdbd06
Your Name <hernandezyoenis@gmail.com>
2023-11-20 13:11:04 -0500

    Passing and defining functions in child class

--- Python/Studying/Lectures/11-7-23/classes.py
+++ Python/Studying/Lectures/11-7-23/classes.py
@@ -9,20 +9,35 @@ class Car:
         long_name = str(self.year) + " " + self.make + " " + self.model
         return long_name.title()
 
-    def read_odometer(self):
+    def Read_odometer(self):
         print("This car has " + str(self.odomoter) + " miles on it.")
+
+    def FillGasTank(self):
+        print("This car has a full gas tank")
 
 class ElectricCar(Car):
     def __init__(self,make,model,year):
         super().__init__(make,model,year) # Creates instance of Car 
+        self.battery_size = 75
         
 
     def charge(self):
         print(f"This {self.model} is charging")
 
+    def FillGasTank(self):
+        print("This car Doesnt have a gas tank")
+
 MyCar = Car('chevy','camaro',2021)
 MyOtherCar = ElectricCar('tesla','model S',2021)
 
+## The child class can still use stuff defined in the parent class
 print(MyCar.get_descriptive_name())
 print(MyOtherCar.get_descriptive_name())
-MyOtherCar.read_odometer()
+# MyOtherCar.Read_odometer()
+
+## The parent class canot use stuff defined in the child class
+# MyCar.charge() 
+
+print("\n\n")
+MyCar.FillGasTank()
+MyOtherCar.FillGasTank()
