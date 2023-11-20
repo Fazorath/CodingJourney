d963c07b0be76865fd5c3d9ab1d4edb8ae6a2a6d
Your Name <hernandezyoenis@gmail.com>
2023-11-20 17:54:47 -0500

    pair header function progress i think

--- Python/Project7/final.py
+++ Python/Project7/final.py
@@ -1,13 +1,13 @@
 ## Yoenis Hernandez
 ## 12/31/2018
-## The goal for this program is to take a CSV file and create JSON strings that can be used in JavaScript 
+## The goal for this program is to take a txt file and create JSON strings that can be used in JavaScript 
 ## for each row of data that is not the header row.  JavaScript is a scripting language used for web pages.  
 ## JSON is a common format used to transmit data for web apps from a web server to a client 
 ## (i.e. such as from sfcollege.edu's web server to your computer) so it can be displayed in a browser.
 ## Create a class and FN
-## Read a CSV
+## Read a txt
 ## Keep CSV header
-## Paid header with the values
+## Pair header with the values
 ## Ceate Json String
 ## Return JSON string to Screen
 ## return JSON string to file
@@ -20,33 +20,48 @@ class CSVtoJSON:
         self.output_fileName = input("Enter the JSON file Name: ")
         ## Holds the Header row of the csv file
         self.header = []
-        ## Holds the data rows of the csv file
+        ## Holds the value of the source data
         self.read_filename = []
-    
+        ## Holds the JSON string
+        self.data = []
+
+
     def read_txt_file(self):
+      
         try: ## Exception handling
             with open(self.filename, "r") as f:
                 file_read = f.readlines() ## Read and create list
+      
         except:
             print(f"{self.filename} not found")
+       
         else:
             self.read_filename = file_read
-            print(file_read)
-
-
-            
-
-    
-
-       ## Create JSON strings
-
+            # print(file_read)
+  
+  
     def assign_header(self):
-        self.header = self.read_filename[0]
+        self.header = self.read_filename[0].strip().split(",")  
+        
+    def pair_header(self):
+        for line in self.read_filename[1:]:
+            info = line.split(",")
+            row = {}
+            
+            for i in range(len(info)):
+                row[self.header[i]] = info[i]
+            self.data.append(row)
 if __name__ == "__main__":
     ## Creates instance of class
     project7 = CSVtoJSON()
+    
     ## Reads the Txt File
     project7.read_txt_file()
+    # print(project7.read_filename)
+   
     ## Assigns the header of the txt file
     project7.assign_header()
-    print(project7.header)
+    # print(project7.header)
+    
+    project7.pair_header()
+    print(project7.data)
