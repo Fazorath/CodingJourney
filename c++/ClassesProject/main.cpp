dbb0986bbbfb1dff8683f244d7e4b7bdad1c5ebe
Your Name <hernandezyoenis@gmail.com>
2023-11-21 13:39:26 -0500

    getting ready for c++ rpoject 7

--- c++/ClassesProject/main.cpp
+++ c++/ClassesProject/main.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
-// Write a C++ program that demonstrates a class named Dice.
-// It will use the random number generator to simulate rolling a 6-sided Die 100 times, 
-// while counting how many times each number of the die comes up.
-//
-// Example output:
-// The number 1 was rolled 23 times
-// The number 2 was rolled 15 times
+// Using OOP, write a C++ program that will indicate the number of days in a month where the temperature was above average, 
+// below average, and at the average for the month of November.  
+// Using an array to count the number of days above average, below average, and at average, 
+// read in an input file named NovTemps.txt that gives the temperature for each day of the month.  
+// The input file should be located in the current directory.  
+// If the temperature is above the average temperature, 
+// increment the counter in the array that holds the count for the days above average.  
+// If the temperature is below the average temperature, increment the counter in the array that holds the count 
+// for the days below average.  If the temperature is equal to the average temperature, increment the counter in 
+// the array that holds the count for the days equal to the average.
+
+// When the end of file has been reached, display the count for the number of days above average, below average, and equal to the average.
+
+// Be sure to place variables within the private accessor area of the class.  
+// Use a constructor to initialize the array elements to 0.  No global variables are allowed.  
+// A submission of all code in main() will result in a grade of 0.
