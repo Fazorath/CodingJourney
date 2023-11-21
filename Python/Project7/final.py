a31bcaefb231df30090a15c46b788eeae3b24564
Your Name <hernandezyoenis@gmail.com>
2023-11-20 22:23:20 -0500

    More final Changes

--- Python/Project7/final.py
+++ Python/Project7/final.py
@@ -1,11 +1,11 @@
 ## Yoenis Hernandez
-## 12/31/2018
+## 11/19/2023
 ## The goal for this program is to take a txt file and create JSON strings that can be used in JavaScript 
 ## for each row of data that is not the header row.  JavaScript is a scripting language used for web pages.  
 ## JSON is a common format used to transmit data for web apps from a web server to a client 
 ## (i.e. such as from sfcollege.edu's web server to your computer) so it can be displayed in a browser.
 ## Create a class and FN
-## Read a txt
+## Read a txt 
 ## Keep CSV header
 ## Pair header with the values
 ## Create Json String
