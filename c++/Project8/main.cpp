ef5968c603633af74637370e4cc8d34de72d172d
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2023-11-29 13:21:03 -0500

    Getting strarted on project8++

---
+++ c++/Project8/main.cpp
@@ -0,0 +1,22 @@
+// Yoenis Hernandez
+// COP 2000
+// 11/28/23
+// read in a file of names
+// read and store the file names into an array of 30 names
+// sort the array using selection sort or buuble sort
+// list the names in ascending alpabetical order
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+
+
+
+
+int main(){
+}
