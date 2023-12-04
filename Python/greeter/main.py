85e48218280392523748fa55d65c4a3c73c26872
Your Name <hernandezyoenis@gmail.com>
2023-12-04 18:22:22 -0500

    learning the console

---
+++ Python/greeter/main.py
@@ -0,0 +1,34 @@
+
+# # Greeter is a terminal application that greets old friends warmly,
+# #   and remembers new friends.
+
+# # Display a title bar.
+# print("\t**********************************************")
+# print("\t***  Greeter - Hello old and new friends!  ***")
+# print("\t**********************************************")
+
+# # Display a bunch of output, representing a long-running program.
+# for x in range(0,51):
+#     print("\nWe have done %d fun and interesting things together!" % x)
+
+from time import sleep
+
+# Greeter is a terminal application that greets old friends warmly,
+#   and remembers new friends.
+
+# Display a title bar.
+print("\t**********************************************")
+print("\t***  Greeter - Hello old and new friends!  ***")
+print("\t**********************************************")
+
+# Print a bunch of information, in short intervals
+names = ['aaron', 'brenda', 'cyrene', 'david', 'eric']
+
+# Print each name 5 times.
+for name in names:
+    # Pause for 1 second between batches, and then skip two lines.
+    sleep(1)
+    print("\n\n")
+    
+    for x in range(0,5):
+        print(name.title())
---
+++ Python/greeter/sleep.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+print("I'm going to sleep now.")
+
+# Sleep for 3 seconds.
+# Basically tells the console to wait till the time is up.
+sleep(3)
+
+print("I woke up!")
