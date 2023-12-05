0ff92a4a9dec5b0f4aa91b9b4ce112f1a799b380
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2023-12-05 18:51:58 -0500

    Lightwork should be a good grade

---
+++ c++/Final/exam.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+// int number = 6;
+// number++;
+// cout << number << endl;
+
+
+// cout << "Four" << "score" << endl;
+
+// cout << "and" << "seven" << endl;
+
+// cout << "years" << "ago" << endl;
+
+// true || true;
+
+
+// int donuts = 10;
+// if (donuts = 1)
+//    donuts = 0;
+// else
+//    donuts += 2;
+
+// cout << donuts << endl;
+
+
+// for (int i = 1; i < 20; i++)
+//    cout << "Hello!" << endl;
+
+// int x = 0, y = 1, z = 2;
+
+// cout << x << y << z << endl;
+
+// int number = 6;
+// cout << ++number << endl;
+
+
+// int x =2;
+// int y = 1;
+// int z = 0;
+// cout << "Answer = " << (x || !y && z) << endl;
+
+// int numbers[4] = { 99, 87 };
+// cout << numbers[3] << endl;
+
+// int number = 10;
+// number += 5;
+// number -= 2;
+// number *= 3;
+// cout << number << endl;
+
+// int score = 40;
+// if (score > 95)
+//    cout << "Congratulations!\n";
+//    cout << "That's a high score!\n";
+//    cout << "This is a test question!" << endl;
+
+// int x = 1, y  = 1, z = 1;
+// y = y + z;
+// x = x + y;
+// cout << "result = "
+//      << (x < y ? y : x)
+//      << endl;
+
+// int w = 98;
+// int x = 99;
+// int y = 0;
+// int z = 1;
+// if (x >= 99)
+// {
+//    if (x < 99)
+//       cout << y << endl;
+//    else
+//       cout << z << endl;
+// }
+// else
+// {
+//    if (x == 99)
+//       cout << x << endl;
+//    else
+//       cout << w << endl;
+// }
+
+// int grades[ ] = { 100, 90, 99, 80 };
+
+}
+//
